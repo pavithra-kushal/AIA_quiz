5133e29082a619e228d29f1603c2916663d4836c
Pavithra K <pavithra.ks29@gmail.com>
2020-04-28 10:16:31 -0500

    Added Congrats msg and changed the gradingscrpit1 2nd question

--- gradingScript1.py
+++ gradingScript1.py
@@ -2,7 +2,7 @@ import hashlib
 
 QUESTIONS = [
         'Question 1) Jenkins is an automation tool used for Continuous Integration (Enter "True" or "False")',
-        'Question 2) Which account did you authenticate for ssh login without password?(Enter the account name)',
+        'Question 2) Which user account did you authenticate for ssh login without password?(Enter the user name)',
         'Question 3) Maven is used for version control (Enter "True" or "False")',
         'Question 4) How many types of mechanisms does Jenkins support for SonarQube authentication (Hint: You used the "Secret Text" mechanism for this lab)? (Enter a number)',
         'Question 5) Security Hotspots in SonarQube are the security-sensitive code that require manual review?(Enter "True" or "False") ',
@@ -11,7 +11,7 @@ QUESTIONS = [
 
 ANSWERS = [
 "b5bea41b6c623f7c09f1bf24dcae58ebab3c0cdd90ad966bc43a45b44867e12b",
-"264c8c381bf16c982a4e59b0dd4c6f7808c51a05f64c35db42cc78a2a72875bb",
+"424e9661130eeeb05edccfd89125d903eafd4e9f9cdf6d0b88fff6b51df92a69",
 "fcbcf165908dd18a9e49f7ff27810176db8e9f63b4352213741664245224f8aa",
 "e7f6c011776e8db7cd330b54174fd76f7d0216b612387a5ffcfb81e6f0919683",
 "b5bea41b6c623f7c09f1bf24dcae58ebab3c0cdd90ad966bc43a45b44867e12b",
@@ -42,3 +42,4 @@ if __name__=="__main__":
             print("Wrong answer :(")
             print "Your score is ", getScore(qno)
             exit()
+    print("Congrats. You have completed the quiz!!!")
